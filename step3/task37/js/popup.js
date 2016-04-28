98c3ee72a468c2d9cdac0f5c9d49905a0289abf9
Xingan Wang <wangxgwxg@gmail.com>
2016-04-28 23:30:27 +0800

    task37 finish popup's header and content

---
+++ step3/task37/js/popup.js
@@ -0,0 +1,141 @@
+/**
+ * addEventHandler方法
+ * 跨浏览器实现事件绑定
+ */
+function addEventHandler(ele, event, hanlder) {
+    if (ele.addEventListener) {
+        ele.addEventListener(event, hanlder, false);
+    } else if (ele.attachEvent) {
+        ele.attachEvent("on"+event, hanlder);
+    } else  {
+        ele["on" + event] = hanlder;
+    }
+}
+
+/**
+ * Widgets Module containing all the widget object available.
+ */
+var Widgets = (function() {
+    /**
+     * popup surface widget to show info
+     * @param  {string} id     the id of the html element generated
+     * @param  {Json} config   the configuration of the popup widget
+     * @return {Object}        the widget object
+     */
+    var popup = function(id, config){
+        if(this instanceof popup)
+        {
+            this.id = id || "POPUP";
+            this.config = config || this.default_config;
+            this.init();
+        }else{
+            return new popup(id, config);
+        }
+    }
+
+    return {
+        popup: popup
+    };
+}());
+
+Widgets.popup.prototype = {
+    // default config map
+    default_config: {
+        width: '300px',
+        height: '200px',
+        title_height: '28px',
+        title: 'Title',
+        content: 'Contents',
+        ismovable: true,//是否可移动
+        isresizable: true,//是否可以改变大小
+    },
+
+    // Create a new popup widget
+    init: function(){
+        // If there isn't one, create one at the start of the document.
+        var popup = document.getElementById(this.id);
+        if (!popup) {
+            popup = document.createElement("div");
+            popup.id = this.id;
+            popup.className = "popup";
+            document.body.insertBefore(popup, document.body.firstChild);
+            // set style
+            ps = popup.style;
+            ps.width = this.config.width;
+            ps.height = this.config.height;
+            ps.background = "#FFFFFF"
+            ps.position = "fixed";
+            ps.left = "50%";
+            ps.top = "50%";
+            ps.marginRight = "-50%";
+            ps.transform = "translate(-50%, -50%)";
+            ps.zIndex = "9999";
+            ps.border = "3px outset gray";
+            // set rest
+            this.setTitle(popup);
+            this.setContent(popup);
+            this.setButton(popup);
+            this.setMask();
+        }
+    },
+
+
+    setTitle: function(popup){
+        var title = document.createElement("header");
+        title.className = "popup-header";
+        title.innerHTML = this.config.title;
+        popup.appendChild(title);
+        // set style
+        ts = title.style;
+        ts.position = "relative";
+        ts.top = "0px";
+        ts.height = this.config.title_height;
+        ts.padding = "3px 5px 2px 5px";
+        ts.background = "#aaa";
+        ts.borderBottom = "2px groove gray"
+        ts.font = "bold 14pt"
+    },
+
+    setContent: function(popup){
+        var content = document.createElement("section");
+        content.className = "popup-content";
+        content.innerHTML = this.config.content;
+        popup.appendChild(content);
+        // set style
+        cs = content.style;
+        cs.position = "relative";
+        cs.height = "155px";
+        cs.padding = "5px";
+        cs.margin = "0px";
+        cs.overflow = "auto";
+        cs.background = "#fff";
+    },
+
+    setButton: function(popup){
+        var confirmBtn = document.createElement("div");
+    },
+
+    // mask that covers the rest area
+    setMask: function(){
+
+    },
+
+    setEvent: function(){
+        // close button
+
+        // drag
+        // resize
+    },
+
+    // Delete the popup widget if exists
+    delete: function(id){
+        var id = id || this.id;
+        var popup = document.getElementById(id);
+        if (popup) {
+            document.body.removeChild(popup);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
